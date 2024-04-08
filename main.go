729d6e6fb84c9ea7f362854f58d0821c94d7b357
Nahuel Luca <nahuelluca20@gmail.com>
2024-04-08 18:06:58 -0300

    add air

--- main.go
+++ main.go
@@ -3,15 +3,14 @@ package main
 import (
 	"net/http"
 
+	"github.com/Nahuelluca20/go-rest-api-letter-box/routes"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/api/v1/movies", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("All Movies"))
-	})
+	r.HandleFunc("/api/v2/movies", routes.HomeHandler)
 
-	http.ListenAndServe(":8080", r)
+	http.ListenAndServe(":3030", r)
 }
