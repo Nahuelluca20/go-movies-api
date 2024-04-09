b1dc8ceba3ebf24d2ae997ac051fe542d61beff2
Nahuel Luca <nahuelluca20@gmail.com>
2024-04-09 15:25:36 -0300

    add post request

--- routes/actors.route.go
+++ routes/actors.route.go
@@ -1,9 +1,12 @@
 package routes
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
+	"github.com/Nahuelluca20/go-rest-api-letter-box/db"
+	"github.com/Nahuelluca20/go-rest-api-letter-box/models"
 	"github.com/gorilla/mux"
 )
 
@@ -14,11 +17,23 @@ func GetActorsHandler(w http.ResponseWriter, r *http.Request) {
 func GetActorByIdHandler(w http.ResponseWriter, r *http.Request) {
 	id := mux.Vars(r)["id"]
 	fmt.Fprintf(w, "Obtener actor con ID: %s", id)
-
 }
 
 func CreateActorHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Create actors")
+	var actor models.Actor
+
+	// &actor -> Pointer to the actor variable
+	json.NewDecoder(r.Body).Decode(&actor)
+
+	createdUser := db.DB.Create(&actor)
+	err := createdUser.Error
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Error when creating the actor"))
+	}
+
+	json.NewEncoder(w).Encode(&actor)
 }
 
 func DeleteActorHandler(w http.ResponseWriter, r *http.Request) {
