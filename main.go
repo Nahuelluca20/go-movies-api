178aa7d841262a5d5495aef720cb80086e17ad90
Nahuel Luca <nahuelluca20@gmail.com>
2024-04-09 13:29:56 -0300

    add skeleton actors routes

--- main.go
+++ main.go
@@ -19,5 +19,11 @@ func main() {
 
 	r.HandleFunc("/api/v2/movies", routes.HomeHandler)
 
+	// Actors Routes
+	r.HandleFunc("/api/v2/actors", routes.GetActorsHandler).Methods("GET")
+	r.HandleFunc("/api/v2/actors/{id}", routes.GetActorByIdHandler).Methods("GET")
+	r.HandleFunc("/api/v2/actors", routes.CreateActorHandler).Methods("POST")
+	r.HandleFunc("/api/v2/actors/{id}", routes.DeleteActorHandler).Methods("GET")
+
 	http.ListenAndServe(":3035", r)
 }
---
+++ routes/actors.route.go
@@ -0,0 +1,30 @@
+package routes
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// func (r *Router) actorsRoutes() {
+
+// }
+
+func GetActorsHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Get actors")
+}
+
+func GetActorByIdHandler(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	fmt.Fprintf(w, "Obtener actor con ID: %s", id)
+
+}
+
+func CreateActorHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Create actors")
+}
+
+func DeleteActorHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Delete actors")
+}
